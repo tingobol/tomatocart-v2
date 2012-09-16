8b2137439d58fc709d27d80771e297ef50f6af24
ray122911@gmail.com <ray122911@gmail.com@8a70709d-6992-197a-dbfa-c327a44bdfb9>
2012-09-16 16:24:32 +0000

    implement service module

--- trunk/system/tomatocart/libraries/service.php
+++ trunk/system/tomatocart/libraries/service.php
@@ -1,57 +1,131 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * TomatoCart Open Source Shopping Cart Solution
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 (2007)
+ * as published by the Free Software Foundation.
+ *
+ * @package		TomatoCart
+ * @author		TomatoCart Dev Team
+ * @copyright	Copyright (c) 2009 - 2012, TomatoCart. All rights reserved.
+ * @license		http://www.gnu.org/licenses/gpl.html
+ * @link		http://tomatocart.com
+ * @since		Version 2.0
+ * @filesource
+ */
 
-class TOC_Service {
+// ------------------------------------------------------------------------
 
-  protected $ci = null;
-  
-  protected $code;
-  protected $title;
-  protected $description;
-  protected $uninstallable = true;
-  protected $depends;
-  protected $precedes;
-  protected $params;
+/**
+ * Registry Class
+ *
+ * @package		TomatoCart
+ * @subpackage	tomatocart
+ * @category	template-module-controller
+ * @author		TomatoCart Dev Team
+ * @link		http://tomatocart.com/wiki/
+ */
+class TOC_Service
+{
+    /**
+     * ci instance
+     *
+     * @access protected
+     * @var object
+     */
+    protected $ci = null;
 
-  // class constructor
-  public function __construct() {
-    // Set the super object to a local variable for use later
-    $this->ci =& get_instance();
+    /**
+     * shipping modules array
+     *
+     * @access protected
+     * @var array
+     */
+    protected $modules = array();
 
-    $this->ci->lang->db_load('modules/services/' . $this->code);
+    /**
+     * each shipping method quotation array
+     *
+     * @access protected
+     * @var string
+     */
+    protected $group = 'shipping';
+
+    /**
+     * Constructor
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct()
+    {
+        // Set the super object to a local variable for use later
+        $this->ci =& get_instance();
+
+        //load extensions model
+        $this->ci->load->model('extensions_model');
+
+        //get installed module
+        $this->modules = $this->ci->extensions_model->get_modules('service');
+        
+        //load module
+        foreach ($this->modules as $module)
+        {
+            $this->ci->load->library('service/service_' . $module);
+        }
+    }
+
+    /**
+     * Is installed 
+     * 
+     * @access public
+     * @param $service
+     * @return boolean
+     */
+    public function is_installed($module)
+    {
+        //check if the language group is loaded
+        if (in_array($module, $this->modules, TRUE))
+        {
+            return TRUE;
+        }
+        
+        return FALSE;
+    }
     
-    $this->title = lang('services_' . $this->code . '_title');
-    $this->description = lang('services_' . $this->code . '_description');
-  }
-  
-  /**
-   * 
-   */
-  public function get_title() {
-    return $this->title;
-  }
-  
-  /**
-   * 
-   */
-  public function get_description() {
-    return $this->description;
-  }
-  
-  /**
-   * 
-   */
-  public function install() {
-  
-  }
-  
-  public function remove() {
-  
-  }
-  
-  public function keys() {
-  }
-  
-  public function run() {
-  }
+    /**
+     * Get service module
+     * 
+     * @access public
+     * @param $module
+     * @return mixed
+     */
+    public function get_service($module)
+    {
+        $service = $this->ci->{'service_' . $module};
+        
+        if (isset($service) && is_object($service))
+        {
+            return $service;
+        }
+        
+        return NULL;
+    }
+    
+    /**
+     * Run service
+     * 
+     * @param $module
+     */
+    public function run($module)
+    {
+        $service = $this->get_service($module);
+        
+        //check whether
+        if (method_exists($service, 'run')) 
+        {
+            $service->run();
+        }
+    }
 }
-
