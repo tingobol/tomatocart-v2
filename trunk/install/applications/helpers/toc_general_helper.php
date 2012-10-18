5c382298ceb7a481c82eaa5ae55e43fca0ab1e6b
ray122911@gmail.com <ray122911@gmail.com@8a70709d-6992-197a-dbfa-c327a44bdfb9>
2012-10-18 10:41:03 +0000

    install initial import

---
+++ trunk/install/applications/helpers/toc_general_helper.php
@@ -0,0 +1,98 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * TomatoCart Open Source Shopping Cart Solution
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 (2007)
+ * as published by the Free Software Foundation.
+ *
+ * @package		TomatoCart
+ * @author		TomatoCart Dev Team
+ * @copyright	Copyright (c) 2009 - 2012, TomatoCart. All rights reserved.
+ * @license		http://www.gnu.org/licenses/gpl.html
+ * @link		http://tomatocart.com
+ * @since		Version 2.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * TomatoCart General Helpers
+ *
+ * @package		TomatoCart
+ * @subpackage	Helpers
+ * @category	Helpers
+ * @author		TomatoCart Dev Team
+ * @link		http://tomatocart.com/wiki/
+ */
+
+/**
+ * Gets a translation
+ *
+ * @access public
+ * @param $key The key to the translation
+ * @return string translation
+ */
+if( ! function_exists('lang'))
+{
+    function lang($key)
+    {
+        $CI =& get_instance();
+
+        return $CI->lang->line($key);
+    }
+}
+
+/**
+ * Gets all languages
+ *
+ * @access public
+ * @return array languages
+ */
+if( ! function_exists('lang_code'))
+{
+    function lang_code()
+    {
+        $CI =& get_instance();
+
+        return $CI->lang->get_code();
+    }
+}
+
+/**
+ * Gets all languages
+ *
+ * @access public
+ * @return array languages
+ */
+if( ! function_exists('get_languages'))
+{
+    function get_languages()
+    {
+        $CI =& get_instance();
+
+        return $CI->lang->get_languages();
+    }
+}
+
+/**
+ * Display language flag
+ *
+ * @access public
+ * @param 
+ * @return array languages
+ */
+if( ! function_exists('get_language_flag'))
+{
+    function get_language_flag($code)
+    {
+        $flag = strtolower(substr($code, 3));
+
+        return '../images/worldflags/' . $flag . '.png';
+    }
+}
+
+
+/* End of file general_helper.php */
+/* Location: ./install/helpers/toc_general_helper.php */
---
+++ trunk/install/applications/views/database.php
@@ -0,0 +1,99 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * TomatoCart Open Source Shopping Cart Solution
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 (2007)
+ * as published by the Free Software Foundation.
+ *
+ * @package		TomatoCart
+ * @author		TomatoCart Dev Team
+ * @copyright	Copyright (c) 2009 - 2012, TomatoCart. All rights reserved.
+ * @license		http://www.gnu.org/licenses/gpl.html
+ * @link		http://tomatocart.com
+ * @since		Version 2.0
+ * @filesource
+ */
+
+  $db_table_types = array('mysql'=> 'MySQL - MyISAM (Default)',
+                          'mysql_innodb'=> 'MySQL - InnoDB (Transaction-Safe)',
+                          'mysqli'=> 'MySQLi (PHP 5 / MySQL 4.1)');
+?>
+<div class="container clearfix">
+    <div class="row-fluid">
+    	<div class="span3">
+            <ul class="nav nav-list">
+                <li class="<?php echo ($step == 1) ? 'active' : ''; ?>"><a href="javascript:void(0);" title="<?php echo lang('nav_menu_step_1_text'); ?>"><i class="icon-chevron-right"></i> <?php echo lang('nav_menu_step_1_text'); ?></a></li>
+                <li class="<?php echo ($step == 2) ? 'active' : ''; ?>"><a href="javascript:void(0);" title="<?php echo lang('nav_menu_step_2_text'); ?>"><i class="icon-chevron-right"></i> <?php echo lang('nav_menu_step_2_text'); ?></a></li>
+                <li class="<?php echo ($step == 3) ? 'active' : ''; ?>"><a href="javascript:void(0);" title="<?php echo lang('nav_menu_step_3_text'); ?>"><i class="icon-chevron-right"></i> <?php echo lang('nav_menu_step_3_text'); ?></a></li>
+                <li class="<?php echo ($step == 4) ? 'active' : ''; ?>"><a href="javascript:void(0);" title="<?php echo lang('nav_menu_step_4_text'); ?>"><i class="icon-chevron-right"></i> <?php echo lang('nav_menu_step_4_text'); ?></a></li>
+                <li class="<?php echo ($step == 5) ? 'active' : ''; ?>"><a href="javascript:void(0);" title="<?php echo lang('nav_menu_step_5_text'); ?>"><i class="icon-chevron-right"></i> <?php echo lang('nav_menu_step_5_text'); ?></a></li>
+                <li class="<?php echo ($step == 6) ? 'active' : ''; ?>"><a href="javascript:void(0);" title="<?php echo lang('nav_menu_step_6_text'); ?>"><i class="icon-chevron-right"></i> <?php echo lang('nav_menu_step_6_text'); ?></a></li>
+			</ul>
+      	  	<div id="mBox">
+      	    	<div id="mBoxContents"></div>
+  	  		</div>  
+    
+            <div id="mainBlock">
+            </div>
+    	</div>
+    	<div class="span9 content">
+          	<h1><?php echo lang('page_title_database_server_setup'); ?></h1>
+          
+          	<p><?php echo lang('text_database_server_setup'); ?></p>
+                  	
+            <form name="install" id="installForm" action="index.php?step=4" method="post" onsubmit="prepareDB(); return false;" class="form-horizontal">
+                <div class="control-group">
+                    <label class="control-label" for="DB_SERVER"><?php echo lang('param_database_server'); ?></label>
+                    <div class="controls">
+                    	<input type="text" id="DB_SERVER" name="DB_SERVER" />
+                    </div>
+                    <div class="description"><?php echo lang('param_database_server_description'); ?></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="DB_SERVER_USERNAME"><?php echo lang('param_database_username'); ?></label>
+                    <div class="controls">
+                    	<input type="text" id="DB_SERVER_USERNAME" name="DB_SERVER_USERNAME" />
+                    </div>
+                    <div class="description"><?php echo lang('param_database_username_description'); ?></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="DB_SERVER_PASSWORD"><?php echo lang('param_database_password'); ?></label>
+                    <div class="controls">
+                    	<input type="password" id="DB_SERVER_PASSWORD" name="DB_SERVER_PASSWORD" />
+                    </div>
+                    <div class="description"><?php echo lang('param_database_password_description'); ?></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="DB_DATABASE"><?php echo lang('param_database_name'); ?></label>
+                    <div class="controls">
+                    	<input type="password" id="DB_DATABASE" name="DB_DATABASE" />
+                    </div>
+                    <div class="description"><?php echo lang('param_database_name_description'); ?></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="DB_DATABASE_CLASS"><?php echo lang('param_database_type'); ?></label>
+                    <div class="controls">
+                    	<?php 
+                    	    echo form_dropdown('DB_DATABASE_CLASS', $db_table_types);
+                    	?>
+                    </div>
+                    <div class="description"><?php echo lang('param_database_type_description'); ?></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="DB_TABLE_PREFIX"><?php echo lang('param_database_prefix'); ?></label>
+                    <div class="controls">
+                    	<input type="text" id="DB_TABLE_PREFIX" name="DB_TABLE_PREFIX" value="toc_" />
+                    </div>
+                    <div class="description"><?php echo lang('param_database_prefix_description'); ?></div>
+                </div>
+                <div class="control-group">
+                    <div class="controls pull-right">
+                    	<a href="javascript:void(0);" class="btn" href="<?php echo site_url(); ?>"><i class="icon-remove"></i> &nbsp;<?php echo lang('image_button_cancel'); ?></a>
+            			<a href="<?php echo site_url('index/index/webserver'); ?>" class="btn"><i class="icon-ok"></i> &nbsp;<?php echo lang('image_button_continue'); ?></a>
+                    </div>
+                </div>
+            </form>
+    	</div>
+	</div>
+</div>
