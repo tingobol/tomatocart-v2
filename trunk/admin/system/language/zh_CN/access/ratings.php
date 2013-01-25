96ec2a8dcbf5ba49ad41229375343e2eabd8f91b
ray122911@gmail.com <ray122911@gmail.com@8a70709d-6992-197a-dbfa-c327a44bdfb9>
2013-01-25 16:08:26 +0000

    update resources

--- trunk/admin/system/language/zh_CN/access/ratings.php
+++ trunk/admin/system/language/zh_CN/access/ratings.php
@@ -1,29 +1,12 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/**
- * TomatoCart
- *
- * An open source application ecommerce framework
- *
- * @package   TomatoCart
- * @author    TomatoCart Dev Team
- * @copyright Copyright (c) 2011, TomatoCart, Inc.
- * @license   http://www.gnu.org/licenses/gpl-3.0.html
- * @link    http://tomatocart.com
- * @since   Version 0.5
- * @filesource ./system/languages/english/ratings.php
- */ 
-?>
+# $Id: quantity_discount_group.php $
+# TomatoCart Open Source Shopping Cart Solutions
+# http://www.tomatocart.com
+#
+# Copyright (c) 2009-2010 Wuxi Elootec Technology Co., Ltd
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License v2 (1991)
+# as published by the Free Software Foundation.
 
-heading_title_ratings = Ratings
+access_ratings_title = 商品评分
 
-table_heading_rating = Rating
-table_heading_action = Action
-table_heading_ratings = Rating Name
-table_heading_status = Status
-
-action_heading_new_rating = New Rating
-
-field_rating_name = Rating Name:
-field_rating_status = Status:
-field_status_enabled = Enabled
-field_status_disabled = Disabled
