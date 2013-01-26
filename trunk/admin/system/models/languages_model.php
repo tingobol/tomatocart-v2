81de17615d3dae7f1357a8a4eef1e039ff93d582
ray122911@gmail.com <ray122911@gmail.com@8a70709d-6992-197a-dbfa-c327a44bdfb9>
2013-01-26 14:55:16 +0000

    update modules

--- trunk/admin/system/models/languages_model.php
+++ trunk/admin/system/models/languages_model.php
@@ -1,101 +1,178 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
- * Ionize, creative CMS
+ * TomatoCart Open Source Shopping Cart Solution
  *
- * @package		Ionize
- * @author		Ionize Dev Team
- * @license		http://ionizecms.com/doc-license
- * @link		http://ionizecms.com
- * @since		Version 0.9.0
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 (2007)
+ * as published by the Free Software Foundation.
+ *
+ * @package   TomatoCart
+ * @author    TomatoCart Dev Team
+ * @copyright Copyright (c) 2009 - 2012, TomatoCart. All rights reserved.
+ * @license   http://www.gnu.org/licenses/gpl.html
+ * @link    http://tomatocart.com
+ * @since   Version 2.0
+ * @filesource
  */
 
 // ------------------------------------------------------------------------
 
 /**
- * Ionize, creative CMS Settings Model
+ * Languages_Model
  *
- * @package		Ionize
- * @subpackage	Models
- * @category	Admin settings
- * @author		Ionize Dev Team
+ * @package   TomatoCart
+ * @subpackage  tomatocart
+ * @category  template-module-model
+ * @author    TomatoCart Dev Team
+ * @link    http://tomatocart.com
  */
-
 class Languages_Model extends CI_Model
 {
-    function __construct()
+    /**
+     * Constructor
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct()
     {
         parent::__construct();
     }
 
+    // ------------------------------------------------------------------------
 
     /**
-     * Get the settings
-     * Don't retrieves the language depending settings
+     * Get the languages resources for specified group
      *
-     * @return	The settings array
+     * @access public
+     * @param $group
+     * @return array
      */
-    function load($group = 'general')
+    public function load($group = 'general')
     {
-        $definitions = array();
-        $this->db->select('*')->from('languages_definitions')->where('languages_id', lang_id())->where('content_group', $group);
-        $qry = $this->db->get();
+        $result = $this->db->select('*')->from('languages_definitions')->where('languages_id', lang_id())->where('content_group', $group)->get();
 
-        foreach ($qry->result() as $key => $row){
+        $definitions = array();
+        foreach ($result->result() as $key => $row)
+        {
             $definitions[$row->definition_key] = $row->definition_value;
         }
 
         return $definitions;
     }
 
-    function get_languages()
+    // ------------------------------------------------------------------------
+
+    /**
+     * Get the system languages
+     *
+     * @access public
+     * @return array
+     */
+    public function get_languages()
     {
         $qry = $this->db->select('*')->from('languages')->order_by(' sort_order, name')->get();
 
         $languages = array();
         foreach ($qry->result_array() as $row){
             $languages[$row['code']] = array( 'id' => $row['languages_id'],
-                                        'code' => $row['code'],
-                                        'country_iso' => strtolower(substr($row['code'], 3)),
-                                        'name' => $row['name'],
-                                        'locale' => $row['locale'],
-                                        'charset' => $row['charset'],
-                                        'date_format_short' => $row['date_format_short'],
-                                        'date_format_long' => $row['date_format_long'],
-                                        'time_format' => $row['time_format'],
-                                        'text_direction' => $row['text_direction'],
-                                        'currencies_id' => $row['currencies_id'],
-                                        'numeric_separator_decimal' => $row['numeric_separator_decimal'],
-                                        'numeric_separator_thousands' => $row['numeric_separator_thousands'],
-                                        'parent_id' => $row['parent_id']);
+                                              'code' => $row['code'],
+                                              'country_iso' => strtolower(substr($row['code'], 3)),
+                                              'name' => $row['name'],
+                                              'locale' => $row['locale'],
+                                              'charset' => $row['charset'],
+                                              'date_format_short' => $row['date_format_short'],
+                                              'date_format_long' => $row['date_format_long'],
+                                              'time_format' => $row['time_format'],
+                                              'text_direction' => $row['text_direction'],
+                                              'currencies_id' => $row['currencies_id'],
+                                              'numeric_separator_decimal' => $row['numeric_separator_decimal'],
+                                              'numeric_separator_thousands' => $row['numeric_separator_thousands'],
+                                              'parent_id' => $row['parent_id']);
         }
 
         return $languages;
     }
 
+    // ------------------------------------------------------------------------
+
     /**
-     * Insert definition
+     * Insert language definition
      *
      * @access public
      * @param $definition
-     * @param $table
      * @return boolean
      */
-    function insert_definition ($definition, $table = 'languages_definitions') {
-        return $this->db->insert($table, $definition);
+    function insert_definition ($definition)
+    {
+        return $this->db->insert('languages_definitions', $definition);
     }
-    
 
+    // ------------------------------------------------------------------------
+
+    /**
+     * Check whether the language definition exists
+     *
+     * @access public
+     * @param $definition
+     * @return boolean
+     */
+    function check_definition($definition) 
+    {
+        $result = $this->db->select('*')->from('languages_definitions')->where($definition)->get();
+
+        if ($result->num_rows() > 0) 
+        {
+            return TRUE;
+        }
+
+        return FALSE;
+    }
+
+    // ------------------------------------------------------------------------
+    
     /**
      * Remove definition
      *
      * @access public
      * @param $definition
-     * @param $table
      * @return boolean
      */
-    function remove_definition ($definition, $table = 'languages_definitions') {
-        return $this->db->delete($table, array('content_group' => $definition['content_group'], 'definition_key' => $definition['definition_key'], 'languages_id' => $definition['languages_id'])); 
+    function remove_definition ($definition) 
+    {
+        return $this->db->delete('languages_definitions', $definition);
+    }
+
+
+    /**
+     * Import module resources
+     *
+     * @param $module
+     */
+    public function import_module_resources($module) {
+        $languages = $this->lang->get_languages();
+
+        foreach ($languages as $key => $value) {
+            $file = realpath(dirname(__FILE__) . '/../../../') . '/system/tomatocart/language/' . $key . '/modules/boxes/' . $module . '.xml';
+
+            if (file_exists($file)) {
+                $resource = simplexml_load_file($file);
+
+                foreach ($resource->definitions->definition as $definition)
+                {
+                    $definition = array(
+                        'definition_key' => (string) $definition->key,
+                        'definition_value' => (string) $definition->value,
+                        'content_group' => (string) $definition->group,
+                        'languages_id' => $value['id']);
+
+                    if (!$this->check_definition($definition)) {
+                        $this->insert_definition($definition);
+                    }
+                }
+            }
+        }
     }
 }
-/* End of file settings_model.php */
-/* Location: ./application/models/settings_model.php */
+/* End of file languages_model.php */
+/* Location: ./admin/system/models/languages_model.php */
