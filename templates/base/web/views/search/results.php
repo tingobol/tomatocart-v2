bf49a6d396190040021db9d72f909415b0b9a02b
ray122911@gmail.com <ray122911@gmail.com@8a70709d-6992-197a-dbfa-c327a44bdfb9>
2013-02-03 17:54:52 +0000

    improve search

--- templates/base/web/views/search/results.php
+++ templates/base/web/views/search/results.php
@@ -6,14 +6,22 @@
  * it under the terms of the GNU General Public License v3 (2007)
  * as published by the Free Software Foundation.
  *
- * @package      TomatoCart
- * @author       TomatoCart Dev Team
- * @copyright    Copyright (c) 2009 - 2012, TomatoCart. All rights reserved.
- * @license      http://www.gnu.org/licenses/gpl.html
- * @link         http://tomatocart.com
- * @since        Version 2.0
+ * @package		TomatoCart
+ * @author		TomatoCart Dev Team
+ * @copyright	Copyright (c) 2009 - 2012, TomatoCart. All rights reserved.
+ * @license		http://www.gnu.org/licenses/gpl.html
+ * @link		http://tomatocart.com
+ * @since		Version 2.0
  * @filesource
 */
+
+    $pagesize_array = array(12 => 12, 24 => 24, 36 => 36, 48 => 48);
+    $sort_array = array('name|asc' => lang('Name (A ~ Z)'), 
+                        'name|desc' => lang('Name (Z ~ A)'), 
+                        'price|asc' => lang('Price (L ~ H)'), 
+                        'price|desc' => lang('Price (H ~ L)'), 
+                        'sku|asc' => lang('SKU (L ~ H)'), 
+                        'sku|desc' => lang('SKU (H ~ L)'));
 ?>
 
 <h1><?php echo lang('search_results_heading'); ?></h1>
@@ -21,39 +29,185 @@
 <?php 
     if (count($products) > 0) : 
 ?>
-
-    <div class="products-list">
+	<div class="products-listing-action">
+		<form id="products-filter-top" class="form-inline" action="<?php echo $filter_form_action; ?>" method="get">
+			<div class="row-fluid">
+				<div class="span4">
+            	    <label><?php echo lang('label_products_per_page'); ?></label>&nbsp;
+            	    <?php echo form_dropdown('pagesize', $pagesize_array, $pagesize, 'id="pagesize-top"'); ?>
+				</div>
+				<div class="span4" align="center">
+            	    <label><?php echo lang('label_select_view'); ?></label>&nbsp;
+                    <div class="btn-group">
+                    	<a class="btn btn-small view-type <?php echo (($view == 'list') ? 'active' : ''); ?>" href="javascript:;"><i class="icon-th-list"></i></a> 
+                    	/ 
+                    	<a class="btn btn-small view-type <?php echo (($view == 'grid') ? 'active' : ''); ?>" href="javascript:;"><i class="icon-th"></i></a>
+						<input type="hidden" id="view-top" name="view" value="<?php echo $view; ?>" />
+                    </div>
+				</div>
+				<div class="span4">
+					<div class="pull-right">
+                	    <label><?php echo lang('label_sorting'); ?></label>&nbsp;
+                        <?php echo form_dropdown('sort', $sort_array, $sort, 'id="sort-top"'); ?>
+					</div>
+				</div>
+			</div>
+			<?php 
+			    if (isset($search_filters) && is_array($search_filters)):
+			        foreach ($search_filters as $name => $value):
+			            echo form_hidden($name, $value);
+			        endforeach;
+			    endif;
+			?>
+        </form>
         <?php 
-            foreach($products as $product) :
+            if (!empty($links)):
         ?>
-        <div class="row-fluid clearfix">
-            <a class="image span2" href="<?php echo site_url('product/' . $product['products_id']); ?>">
-    			<img alt="<?php echo $product['products_name']; ?>" title="<?php echo $product['products_name']; ?>" src="<?php echo product_image_url($product['image']); ?>" />
-            </a>
-            <div class="info span6">
-                <a href="<?php echo site_url('product/' . $product['products_id']); ?>"><?php echo $product['products_name']; ?></a>
-                <p class="description">
-                    <?php echo $product['products_short_description']; ?>
-                </p>
-            </div>  
-            <span class="price center span2"><?php echo currencies_format($product['products_price']); ?></span>
-            <div class="buttons span2">
-                <a class="btn btn-small btn-small btn-small btn-info" href="<?php echo site_url('cart_add/' . $product['products_id']); ?>">
-                	<i class="icon-shopping-cart icon-white "></i> 
-                	<?php echo lang('button_add_to_cart'); ?>
-                </a><br />
-                <a class="wishlist" href="javascript:void(0);"><?php echo lang('add_to_wishlist'); ?></a><br />
-                <a class="compare" href="javascript:void(0);"><?php echo lang('add_to_compare'); ?></a>
-            </div>
+        <div class="seperator"></div>
+        <div class="row-fluid">
+        	<div class="span6 total-pages">
+        		<?php  echo $total_pages; ?>
+        	</div>
+        	<div class="span6">
+                <div class="pagination clearfix">
+                	<?php  echo $links; ?>
+                </div>        	
+        	</div>
         </div>
         <?php 
+            endif;
+        ?>
+	</div>
+	
+    <ul class="products-list <?php echo $view; ?> clearfix">
+        <?php 
+            foreach($products as $product):
+        ?>
+        <li class="clearfix">
+    	<?php 
+    	    if ($product['is_specials'] === TRUE):
+    	?>
+    		<div class="specials-banner"></div>
+    	<?php   
+    	    elseif ($product['is_featured'] === TRUE):  
+    	?>
+    		<div class="featured-banner"></div>
+    	<?php   
+    	    endif;
+    	?>
+            <div class="left">
+                 <a href="<?php echo site_url('product/' . $product['products_id']); ?>">
+                      <img alt="<?php echo $product['product_name']; ?>" title="<?php echo $product['product_name']; ?>" src="<?php echo product_image_url($product['product_image']); ?>" alt="default thumb" class="thumb" />
+                 </a>
+                <h3>
+                	<a href="<?php echo site_url('product/' . $product['products_id']); ?>">
+                        <?php echo $product['product_name']; ?>
+                    </a>
+                </h3>
+                <p class="description">
+                    <?php echo $product['short_description']; ?>
+                </p>
+            </div>
+            <div class="right">
+                <span class="price">
+                	<?php 
+                	    if ($product['is_specials'] === TRUE):
+                	?>
+                		<s><?php echo currencies_format($product['specials_price']); ?></s>
+                	<?php     
+                	    endif;
+                	?>
+                    <?php echo currencies_format($product['product_price']); ?></span>
+                <span class="buttons">
+                    <a class="btn btn-small btn-info" href="<?php echo site_url('cart_add/' . $product['products_id']); ?>">
+                    	<i class="icon-shopping-cart icon-white "></i> 
+                    	<?php echo lang('button_buy_now'); ?>
+                    </a><br />
+                    <a class="wishlist" href="javascript:void(0);"><?php echo lang('add_to_wishlist'); ?></a><br />
+                    <a class="compare" href="javascript:void(0);"><?php echo lang('add_to_compare'); ?></a>
+                </span>
+            </div>
+        </li>
+        <?php
             endforeach;
         ?>
-    </div>
-    
-    <div class="pagination clearfix">
-    	<?php  echo $links; ?>
-    </div>
+    </ul>
+	<div class="products-listing-action">
+		<form id="products-filter-bottom" class="form-inline" action="<?php echo $filter_form_action; ?>" method="get">
+			<div class="row-fluid">
+				<div class="span4">
+            	    <label><?php echo lang('label_products_per_page'); ?></label>&nbsp;
+            	    <?php echo form_dropdown('pagesize', $pagesize_array, $pagesize, 'id="pagesize-bottom"'); ?>
+				</div>
+				<div class="span4" align="center">
+            	    <label><?php echo lang('label_select_view'); ?></label>&nbsp;
+                    <div class="btn-group">
+                    	<a class="btn btn-small view-type <?php echo (($view == 'list') ? 'active' : ''); ?>" href="javascript:;"><i class="icon-th-list"></i></a> 
+                    	/ 
+                    	<a class="btn btn-small view-type <?php echo (($view == 'grid') ? 'active' : ''); ?>" href="javascript:;"><i class="icon-th"></i></a>
+						<input type="hidden" id="view-bottom" name="view" value="<?php echo $view; ?>" />
+                    </div>							
+				</div>
+				<div class="span4">
+					<div class="pull-right">
+                	    <label><?php echo lang('label_sorting'); ?></label>&nbsp;
+                        <?php echo form_dropdown('sort', $sort_array, $sort, 'id="sort-bottom"'); ?>
+					</div>
+				</div>
+			</div>
+			<?php 
+			    if (isset($search_filters) && is_array($search_filters)):
+			        foreach ($search_filters as $name => $value):
+			            echo form_hidden($name, $value);
+			        endforeach;
+			    endif;
+			?>
+        </form>
+        <?php 
+            if (!empty($links)):
+        ?>
+        <div class="seperator"></div>
+        <div class="row-fluid">
+        	<div class="span6 total-pages">
+        		<?php  echo $total_pages; ?>
+        	</div>
+        	<div class="span6">
+                <div class="pagination clearfix">
+                	<?php  echo $links; ?>
+                </div>        	
+        	</div>
+        </div>
+        <?php 
+            endif;
+        ?>
+	</div>
+	<script type="text/javascript">
+    <!--
+    $(function() {
+		$('#pagesize-top, #pagesize-bottom, #sort-top, #sort-bottom').select2().on('change', function(o) {
+		    var pos = '-top';
+			if (o.currentTarget.id.indexOf('-bottom') > 0) {
+			    pos = '-bottom';
+			}
+			$('#products-filter' + pos).submit();
+		});
+
+    	$('.view-type').on('click', function(object) {
+        	var $this = $(this);
+        	
+        	if (!$this.hasClass('active')) {
+            	var view = 'grid';
+				if ($this.find('.icon-th-list').length > 0) {
+					var view = 'list';
+				}
+
+				$this.parent().find('input').val(view);
+				$this.parents('form').submit();
+            }
+    	});
+    });
+    //-->
+    </script>
 <?php 
     endif; 
 ?>
