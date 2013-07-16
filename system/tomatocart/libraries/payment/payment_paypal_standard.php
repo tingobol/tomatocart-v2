9f22ee3d5e3834b735db261143435d50f3bee88d
ray122911@gmail.com <ray122911@gmail.com@8a70709d-6992-197a-dbfa-c327a44bdfb9>
2013-07-16 19:07:41 +0000

    payment

--- system/tomatocart/libraries/payment/payment_paypal_standard.php
+++ system/tomatocart/libraries/payment/payment_paypal_standard.php
@@ -45,111 +45,111 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
      * @var array
      */
     var $params = array(
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_STATUS',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_STATUS',
               'title' => 'Enable PayPal Website Payments Standard', 
               'type' => 'combobox',
               'mode' => 'local',
               'value' => 'True',
               'description' => 'Do you want to accept PayPal Website Payments Standard payments?',
               'values' => array(array('id' => 'True', 'text' => 'True'),
-                                array('id' => 'False', 'text' => 'False'))),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_ID',
+    array('id' => 'False', 'text' => 'False'))),
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_ID',
               'title' => '* PayPal E-Mail Address', 
               'type' => 'textfield',
 		   	  'value' => '',
               'description' => 'The seller e-mail address to use for accepting PayPal payments.'),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_ZONE',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_ZONE',
               'title' => 'Payment Zone', 
               'type' => 'combobox',
               'mode' => 'remote',
 		   	  'value' => '0',
               'description' => 'If a zone is selected, only enable this payment method for that zone.',
               'action' => 'config/get_shipping_zone'),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_SORT_ORDER',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_SORT_ORDER',
               'title' => 'Sort order of display.', 
               'type' => 'numberfield',
               'value' => '0',
               'description' => 'Sort order of display. Lowest is displayed first.'),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_ORDER_STATUS_ID',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_ORDER_STATUS_ID',
               'title' => '* Set PayPal Acknowledged Order Status', 
               'type' => 'combobox',
               'mode' => 'remote',
 		   	  'value' => '0',
               'description' => 'Set the status of orders made with this payment module to this value',
               'action' => 'config/get_order_status'),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_GATEWAY_SERVER',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_GATEWAY_SERVER',
               'title' => '* Gateway Server', 
               'type' => 'combobox',
               'mode' => 'local',
               'value' => 'Sandbox',
               'description' => 'Use the testing (sandbox) or live gateway server for transactions?',
               'values' => array(array('id' => 'Live', 'text' => 'Live'),
-                                array('id' => 'Sandbox', 'text' => 'Sandbox'))),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_TRANSFER_CART',
+    array('id' => 'Sandbox', 'text' => 'Sandbox'))),
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_TRANSFER_CART',
               'title' => 'Transfer Cart Line Items', 
               'type' => 'combobox',
               'mode' => 'local',
               'value' => '1',
               'description' => 'Do you want to transfer the details about the items in the cart to paypal?',
               'values' => array(array('id' => '1', 'text' => 'True'),
-                                array('id' => '-1', 'text' => 'False'))),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_IPN_EWP_PRIVATE_KEY',
+    array('id' => '-1', 'text' => 'False'))),
+    array('name' => 'MODULE_PAYMENT_PAYPAL_IPN_EWP_PRIVATE_KEY',
               'title' => 'Encrypted Web Payments Private Key (Seller)', 
               'type' => 'textfield',
 		   	  'value' => '',
               'description' => 'The location of your Private Key to use for signing the data. (*.pem)'),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_TRANSACTION_METHOD',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_TRANSACTION_METHOD',
               'title' => 'Transaction Method', 
               'type' => 'combobox',
               'mode' => 'local',
               'value' => 'Sale',
               'description' => 'The processing method to use for each transaction.',
               'values' => array(array('id' => 'Authorization', 'text' => 'Authorization'),
-                                array('id' => 'Sale', 'text' => 'Sale'))),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_PAGE_STYLE',
+    array('id' => 'Sale', 'text' => 'Sale'))),
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_PAGE_STYLE',
               'title' => 'Page Style', 
               'type' => 'textfield',
 		   	  'value' => '',
               'description' => 'The page style to use for the transaction procedure (defined at your PayPal Profile page)'),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_DEBUG_EMAIL',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_DEBUG_EMAIL',
               'title' => 'Debug E-Mail Address', 
               'type' => 'textfield',
 		   	  'value' => '',
               'description' => 'All parameters of an Invalid IPN notification will be sent to this email address if one is entered.'),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_STATUS',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_STATUS',
               'title' => 'Enable Encrypted Web Payments', 
               'type' => 'combobox',
               'mode' => 'local',
               'value' => 'Sale',
               'description' => 'Do you want to enable Encrypted Web Payments?',
               'values' => array(array('id' => '1', 'text' => 'True'),
-                                array('id' => '-1', 'text' => 'False'))),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_PRIVATE_KEY',
+    array('id' => '-1', 'text' => 'False'))),
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_PRIVATE_KEY',
               'title' => 'Your Private Key', 
               'type' => 'textfield',
 		   	  'value' => '',
               'description' => 'The location of your Private Key to use for signing the data. (*.pem)'),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_PUBLIC_KEY',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_PUBLIC_KEY',
               'title' => 'Your Public Certificate', 
               'type' => 'textfield',
 		   	  'value' => '',
               'description' => 'The location of your Public Certificate to use for signing the data. (*.pem)'),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_PAYPAL_KEY',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_PAYPAL_KEY',
               'title' => 'PayPals Public Certificate', 
               'type' => 'textfield',
 		   	  'value' => '',
               'description' => 'The location of the PayPal Public Certificate for encrypting the data.'),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_CERT_ID',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_CERT_ID',
               'title' => 'Your PayPal Public Certificate ID', 
               'type' => 'textfield',
 		   	  'value' => '',
               'description' => 'The Certificate ID to use from your PayPal Encrypted Payment Settings Profile.'),
-        array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_OPENSSL',
+    array('name' => 'MODULE_PAYMENT_PAYPAL_STANDARD_EWP_OPENSSL',
               'title' => 'OpenSSL Location', 
               'type' => 'textfield',
 		   	  'value' => '/usr/bin/openssl',
               'description' => 'The location of the openssl binary file.'));
-        
+
     /**
      * ignore order totals module
      *
@@ -157,7 +157,7 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
      * @var array
      */
     private $ignore_order_totals = array('sub_total', 'tax', 'total');
-            
+
     /**
      * Transaction Response
      *
@@ -189,8 +189,6 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
      */
     function initialize()
     {
-        $this->ci->load->model('address_model');
-
         if ($this->config['MODULE_PAYMENT_PAYPAL_STANDARD_GATEWAY_SERVER'] == 'Live')
         {
             $this->form_action_url = 'https://www.paypal.com/cgi-bin/webscr';
@@ -203,13 +201,13 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
         if ($this->status === TRUE)
         {
             $this->order_status = $this->config['MODULE_PAYMENT_PAYPAL_STANDARD_ORDER_STATUS_ID'] > 0 ? (int) $this->config['MODULE_PAYMENT_PAYPAL_STANDARD_ORDER_STATUS_ID'] : (int)config('ORDERS_STATUS_PAID');
-            
+
             if ((int)$this->config['MODULE_PAYMENT_PAYPAL_STANDARD_ZONE'] > 0)
             {
                 $this->ci->load->model('address_model');
-                
+
                 $zones = $this->ci->address_model->get_zone_id_via_geo_zone($this->ci->shopping_cart->get_billing_address('country_id'), $this->config['MODULE_PAYMENT_PAYPAL_STANDARD_ZONE']);
-                
+
                 $check_flag = FALSE;
                 if ($zones !== NULL)
                 {
@@ -254,18 +252,18 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
      */
     function confirmation()
     {
-        $this->ci->load->model('order_model');
-
-        $this->order_id = $this->ci->order_model->insert_order();
+        $this->ci->load->library('order');
+        
+        $this->order_id = $this->ci->order->create_order(ORDERS_STATUS_PREPARING);
     }
 
     /**
      * Process button
-     * 
+     *
      * @access public
      * @return string
      */
-    function process_button() 
+    function process_button()
     {
         $process_button_string = '';
         $params = array('business' => $this->config['MODULE_PAYMENT_PAYPAL_STANDARD_ID'],
@@ -280,7 +278,7 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
                       'bn' => 'Tomatocart_Default_ST',
                       'paymentaction' => (($this->config['MODULE_PAYMENT_PAYPAL_STANDARD_TRANSACTION_METHOD'] == 'Sale') ? 'sale' : 'authorization'));
 
-        if ($this->ci->shopping_cart->has_shipping_address()) 
+        if ($this->ci->shopping_cart->has_shipping_address())
         {
             $params['address_override'] = '1';
             $params['first_name'] = $this->ci->shopping_cart->get_shipping_address('firstname');
@@ -290,8 +288,8 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
             $params['state'] = $this->ci->shopping_cart->get_shipping_address('zone_code');
             $params['zip'] = $this->ci->shopping_cart->get_shipping_address('postcode');
             $params['country'] = $this->ci->shopping_cart->get_shipping_address('country_iso_code_2');
-        } 
-        else 
+        }
+        else
         {
             $params['no_shipping'] = '1';
             $params['first_name'] = $this->ci->shopping_cart->get_billing_address('firstname');
@@ -303,18 +301,18 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
             $params['country'] = $this->ci->shopping_cart->get_billing_address('country_iso_code_2');
         }
 
-        if ($this->config['MODULE_PAYMENT_PAYPAL_STANDARD_TRANSFER_CART'] == '-1') 
+        if ($this->config['MODULE_PAYMENT_PAYPAL_STANDARD_TRANSFER_CART'] == '-1')
         {
             $params['cmd'] = '_xclick';
             $params['item_name'] = config('STORE_NAME');
 
             $shipping_tax = ($this->ci->shopping_cart->get_shipping_method('cost')) * ($this->ci->tax->get_tax_rate($this->ci->shopping_cart->get_shipping_method('tax_class_id'), $this->ci->shopping_cart->get_taxing_address('country_id'), $this->ci->shopping_cart->get_taxing_address('zone_id')) / 100);
 
-            if (config('DISPLAY_PRICE_WITH_TAX') == '1') 
+            if (config('DISPLAY_PRICE_WITH_TAX') == '1')
             {
                 $shipping = $this->ci->shopping_cart->get_shipping_method('cost');
-            } 
-            else 
+            }
+            else
             {
                 $shipping = $this->ci->shopping_cart->get_shipping_method('cost') + $shipping_tax;
             }
@@ -324,11 +322,11 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
             $params['tax'] = $this->ci->currencies->format_raw($total_tax);
             $params['amount'] = $this->ci->currencies->format_raw($this->ci->shopping_cart->get_total() - $shipping - $total_tax);
         }
-        else 
+        else
         {
             $params['cmd'] = '_cart';
             $params['upload'] = '1';
-            if (config('DISPLAY_PRICE_WITH_TAX') == '-1') 
+            if (config('DISPLAY_PRICE_WITH_TAX') == '-1')
             {
                 $params['tax_cart'] = $this->ci->currencies->format_raw($this->ci->shopping_cart->getTax());
             }
@@ -339,23 +337,23 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
                 $i = 1;
 
                 $products = $this->ci->shopping_cart->get_products();
-                foreach($products as $product) 
+                foreach($products as $product)
                 {
                     $product_name = $product['name'];
 
                     //gift certificate
-                    if ($product['type'] == PRODUCT_TYPE_GIFT_CERTIFICATE) 
+                    if ($product['type'] == PRODUCT_TYPE_GIFT_CERTIFICATE)
                     {
                         $product_name .= "\n" . ' - ' . lang('senders_name') . ': ' . $product['gc_data']['senders_name'];
 
-                        if ($product['gc_data']['type'] == GIFT_CERTIFICATE_TYPE_EMAIL) 
+                        if ($product['gc_data']['type'] == GIFT_CERTIFICATE_TYPE_EMAIL)
                         {
                             $product_name .= "\n" . ' - ' . lang('senders_email')  . ': ' . $product['gc_data']['senders_email'];
                         }
 
                         $product_name .= "\n" . ' - ' . lang('recipients_name') . ': ' . $product['gc_data']['recipients_name'];
 
-                        if ($product['gc_data']['type'] == GIFT_CERTIFICATE_TYPE_EMAIL) 
+                        if ($product['gc_data']['type'] == GIFT_CERTIFICATE_TYPE_EMAIL)
                         {
                             $product_name .= "\n" . ' - ' . lang('recipients_email')  . ': ' . $product['gc_data']['recipients_email'];
                         }
@@ -363,9 +361,9 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
                         $product_name .= "\n" . ' - ' . lang('message')  . ': ' . $product['gc_data']['message'];
                     }
 
-                    if ($this->ci->shopping_cart->has_variants($product['id'])) 
+                    if ($this->ci->shopping_cart->has_variants($product['id']))
                     {
-                        foreach ($this->ci->shopping_cart->get_variants($product['id']) as $variant) 
+                        foreach ($this->ci->shopping_cart->get_variants($product['id']) as $variant)
                         {
                             $product_name .= ' - ' . $variant['groups_name'] . ': ' . $variant['values_name'];
                         }
@@ -384,15 +382,15 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
             }
 
             //order totals
-            foreach ($this->ci->shopping_cart->get_order_totals() as $total) 
+            foreach ($this->ci->shopping_cart->get_order_totals() as $total)
             {
-                if ( !in_array($total['code'], $this->ignore_order_totals) ) 
+                if ( !in_array($total['code'], $this->ignore_order_totals) )
                 {
-                    if ( ($total['code'] == 'coupon') || ($total['code'] == 'gift_certificate') ) 
+                    if ( ($total['code'] == 'coupon') || ($total['code'] == 'gift_certificate') )
                     {
                         $params['discount_amount_cart'] += $this->ci->currencies->format_raw(abs($total['value']));
-                    } 
-                    else 
+                    }
+                    else
                     {
                         $order_total = array('item_name_' . $i => $total['title'], 'quantity_' . $i => 1, 'amount_' . $i => $total['value']);
                         $params = array_merge($params, $order_total);
@@ -482,8 +480,41 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
     }
 
     function process() {
-        if (isset($_POST['invoice']) && is_numeric($_POST['invoice']) && isset($_POST['receiver_email']) && ($_POST['receiver_email'] == config('MODULE_PAYMENT_PAYPAL_STANDARD_ID')) && isset($_POST['verify_sign']) && (empty($_POST['verify_sign']) === FALSE) && isset($_POST['txn_id']) && (empty($_POST['txn_id']) === FALSE)) {
-            unset($_SESSION['prepOrderID']);
+        $pre_order_id = $this->ci->session->userdata('pre_order_id');
+
+        if ($pre_order_id !== NULL)
+        {
+            $prep = explode('-', $pre_order_id);
+
+            if ($prep[0] == $this->ci->shopping_cart->get_cart_id())
+            {
+                $this->ci->load->model('order_model');
+
+                $orders_status_history = $this->ci->order_model->get_order_status_history($prep[1]);
+
+                $paid = FALSE;
+                if ($orders_status_history !== NULL)
+                {
+                    foreach ($orders_status_history as $orders_status)
+                    {
+                        if ($orders_status['orders_status_id'] == $this->order_status)
+                        {
+                            $paid = TRUE;
+                        }
+                    }
+                }
+
+                if ($paid === FALSE) {
+                    if (!empty($this->config['MODULE_PAYMENT_PAYPAL_STANDARD_PROCESSING_ORDER_STATUS_ID']))
+                    {
+                        $invoice = $this->ci->input->post('invoice');
+                        
+                        $this->ci->order->process($invoice, $this->config['MODULE_PAYMENT_PAYPAL_STANDARD_PROCESSING_ORDER_STATUS_ID'], 'PayPal Processing Transaction');
+                    }
+                }
+            }
+
+            $this->ci->session->unset_userdata('pre_order_id');
         }
     }
 
@@ -574,4 +605,3 @@ class TOC_Payment_paypal_standard extends TOC_Payment_Module
         }
     }
 }
-?>
