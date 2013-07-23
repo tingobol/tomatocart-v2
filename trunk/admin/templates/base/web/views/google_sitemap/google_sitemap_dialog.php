ac55caa35fb83ad44df6cd06a1d03721cb355319
jack.yin@tomatocart.com <jack.yin@tomatocart.com@8a70709d-6992-197a-dbfa-c327a44bdfb9>
2013-07-23 03:21:26 +0000

    [V2.0 Alpha4] Hide sitemap

--- trunk/admin/templates/base/web/views/google_sitemap/google_sitemap_dialog.php
+++ trunk/admin/templates/base/web/views/google_sitemap/google_sitemap_dialog.php
@@ -14,17 +14,16 @@
 Ext.define('Toc.google_sitemap.GoogleSitemapDialog', {
   extend: 'Ext.Window', 
   constructor: function(config) {
-  	
     config = config || {};
-    
+      
     config.id = 'google_sitemap-win';
-	config.title = '<?php echo lang('heading_title'); ?>';
-	config.width = 600;
-	config.height = 440;
-	config.iconCls = 'icon-google_sitemap-win';
-	config.layout = 'fit';
-	config.items = this.buildForm();
-  
+  	config.title = '<?php echo lang('heading_title'); ?>';
+  	config.width = 600;
+  	config.height = 440;
+  	config.iconCls = 'icon-google_sitemap-win';
+  	config.layout = 'fit';
+  	config.items = this.buildForm();
+    
   	config.buttons = [{
       text: TocLanguage.btnClose,
       handler: function() { 
@@ -32,86 +31,93 @@ Ext.define('Toc.google_sitemap.GoogleSitemapDialog', {
       },
       scope: this
     }];
-    
-    this.callParent([config]);
+      
+  this.callParent([config]);
   },
   
   buildForm: function() {
-  var dataStore = Ext.create('Ext.data.Store', {
+    var dataStore = Ext.create('Ext.data.Store', {
       fields: ['id', 'text'],
       data: [
-         {id:"daily",text:"<?php echo lang('Daily'); ?>"},
-         {id:"monthly",text:"<?php echo lang('Monthly'); ?>"},
-         {id:"yearly",text:"<?php echo lang('Yearly'); ?>"}
+         {id:"daily",text:"<?php echo lang('field_daily'); ?>"},
+         {id:"weekly",text:"<?php echo lang('field_weekly'); ?>"},
+         {id:"monthly",text:"<?php echo lang('field_month'); ?>"},
+         {id:"yearly",text:"<?php echo lang('field_year'); ?>"}
        ]
     });
+    
     this.fsCreateSitemap = Ext.create('Ext.form.FieldSet', {
       title: '<?php echo lang('button_create_sitemaps'); ?>',
       autoHeight: true,      
       items: [
-      { xtype: 'panel',
-        layout: 'column',
-        labelWidth: 130,
-        border: false,
-        items: [
-       {  
-       		
-       	  xtype: 'panel',
-          columnWidth: 0.55,
+        { 
+          xtype: 'panel',
+          layout: 'column',
           labelWidth: 130,
           border: false,
-          labelSeparator: ' ',
-          defaultType: 'combo',   
-          defaults: {
-          	xtype: 'combo', store: dataStore, mode: 'local', 
-          	valueField: 'id', 
-          	displayField: 'text', 
-          	value: 'daily', allowBlank: false, 
-          	editable: false, triggerAction: 'all', anchor: '90%'
-          },
-          items: [ 
-          	{            
-            	fieldLabel: '<?php echo lang("field_categories"); ?>', 
-            	hiddenName: 'categories_frequency' 
-            }, 
-            {fieldLabel: '<?php echo lang("field_products"); ?>', hiddenName: 'products_frequency'},
-            {fieldLabel: '<?php echo lang("field_articles"); ?>', hiddenName: 'articles_frequency'}
+          items: [
+            {  
+              xtype: 'panel',
+              columnWidth: 0.55,
+              labelWidth: 130,
+              border: false,
+              labelSeparator: ' ',
+              defaultType: 'combo',   
+              defaults: {
+              	xtype: 'combo', 
+              	store: dataStore, 
+            	  queryMode: 'local', 
+              	valueField: 'id', 
+              	displayField: 'text', 
+              	value: 'daily', 
+              	allowBlank: false, 
+              	editable: false, 
+              	triggerAction: 'all', 
+              	anchor: '90%'
+              },
+              items: [ 
+              	{            
+                	fieldLabel: '<?php echo lang("field_categories"); ?>', 
+                	hiddenName: 'categories_frequency' 
+                }, 
+                {fieldLabel: '<?php echo lang("field_products"); ?>', hiddenName: 'products_frequency'},
+                {fieldLabel: '<?php echo lang("field_articles"); ?>', hiddenName: 'articles_frequency'}
+              ] 
+            },
+            { 
+              xtype: 'panel',
+              columnWidth: 0.45,
+              border: false,
+              labelSeparator: ' ',
+              defaultType: 'numberfield',
+              labelWidth: 40,
+              defaults: { 
+            	fieldLabel: '<?php echo lang('field_priority'); ?>', 
+            	decimalPrecision: 2, 
+            	allowNegative: false, 
+            	allowBlank: false, 
+            	maxValue: 1, 
+            	minValue: 0, 
+            	anchor: '90%'
+            	},
+            	items: 
+            	[
+                  {name: 'categories_priority', value: 0.5}, 
+                  {name: 'products_priority', value: 0.5}, 
+                  {name: 'articles_priority', value: 0.25}
+              ]
+            }
           ] 
-       },
-       { 
-       	xtype: 'panel',
-       	columnWidth: 0.45,
-          border: false,
-          labelSeparator: ' ',
-          defaultType: 'numberfield',
-          labelWidth: 40,
-          defaults: { 
-          	fieldLabel: '<?php echo lang('field_priority'); ?>', 
-          	decimalPrecision: 2, 
-          	allowNegative: false, 
-          	allowBlank: false, 
-          	maxValue: 1, 
-          	minValue: 0, 
-          	anchor: '90%'
-          	},
-          	items: [
-            {name: 'categories_priority', value: 0.5}, 
-            {name: 'products_priority', value: 0.5}, 
-            {name: 'articles_priority', value: 0.25}
-          ]
-       }
-      ] },
-      {
-	      	xtype: 'button',
+        },
+        {
+        	xtype: 'button',
       		text: '<?php echo lang('button_create_sitemaps'); ?>',
       		handler: function(){
         		this.createSitemap();
       		},
       		scope:this
-      		
       	}
       ]
-    
     });
     
     this.fsSubmitSitemap = Ext.create('Ext.form.FieldSet',{
@@ -123,14 +129,14 @@ Ext.define('Toc.google_sitemap.GoogleSitemapDialog', {
         hideLabel: true, 
         encodeHtml:false, 
         value: '<?php echo lang('introduction_google_sitemaps_submission'); ?>'
-        },
+      },
       {
-      xtype: 'button',
-      text: '<?php echo lang('button_submit_sitemaps'); ?>',
-      		handler: function(){
-        		this.submitSitemap();
-      		},
-      		scope:this
+        xtype: 'button',
+        text: '<?php echo lang('button_submit_sitemaps'); ?>',
+    		handler: function(){
+      		this.submitSitemap();
+    		},
+    		scope:this
       }]
     });
   
